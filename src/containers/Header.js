d4f07a95555077ea182efc544414c5362a0074f8
Raviteja-Tenali <87232036+Raviteja-Tenali@users.noreply.github.com>
2022-10-13 20:12:10 +0530

    Add files via upload

---
+++ src/containers/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ProductListing from './ProductListing';
+
+
+const Header = () => {
+  return (
+    <div className='ui fixed menu bg-warning'>
+        <div className='ui container center'>
+            <h2><b>AMIGOS SHOPPING ZONE</b></h2>
+        </div>
+
+    </div>
+  )
+}
+
+export default Header;
